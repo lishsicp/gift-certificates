4377599b2dc755a7536d8be6786576d362caa178
Yaroslav Lobur <lobur13@gmail.com>
2023-01-08 18:40:50 +0200

    Refactor validate method, added empty certificate check

--- service/src/main/java/com/epam/esm/service/validator/GiftCertificateUpdateValidator.java
+++ service/src/main/java/com/epam/esm/service/validator/GiftCertificateUpdateValidator.java
@@ -18,36 +18,61 @@ public class GiftCertificateUpdateValidator {
     private static final int BAD_CERTIFICATE_DESCRIPTION = 40004;
     private static final int BAD_CERTIFICATE_PRICE = 40005;
     private static final int BAD_CERTIFICATE_DURATION = 40006;
+    private static final int BAD_CERTIFICATE_EMPTY = 40009;
 
     public static void validate(GiftCertificate giftCertificate) throws IncorrectUpdateValueException {
+        checkIfEmpty(giftCertificate);
+        if (giftCertificate.getId() <= 0) {
+            throw new IncorrectUpdateValueException(40001);
+        }
         if (giftCertificate.getName() != null) {
-            String name = giftCertificate.getName();
-            if (name.isBlank() ||
-                    !Pattern.compile("[\\w\\s]{2,128}").matcher(name).matches()) {
-                throw new IncorrectUpdateValueException(BAD_CERTIFICATE_NAME);
-            }
+            checkName(giftCertificate.getName());
         }
         if (giftCertificate.getDescription() != null) {
-            String description = giftCertificate.getDescription();
-            if (description.isBlank() ||
-                    !Pattern.compile("[\\w\\s]{2,512}").matcher(description).matches()) {
-                throw new IncorrectUpdateValueException(BAD_CERTIFICATE_DESCRIPTION);
-            }
+            checkDescription(giftCertificate.getDescription());
         }
         if (giftCertificate.getPrice() != null) {
-            BigDecimal price = giftCertificate.getPrice();
-            if (price == null || price.scale() > MAX_SCALE
-                    || price.compareTo(MIN_PRICE) < 0 || price.compareTo(MAX_PRICE) > 0) {
-                throw new IncorrectUpdateValueException(BAD_CERTIFICATE_PRICE);
-            }
+            checkPrice(giftCertificate.getPrice());
         }
         if (giftCertificate.getDuration() > 0) {
-            int duration = giftCertificate.getDuration();
-            if (duration < 1 || duration > 365) {
-                throw new IncorrectUpdateValueException(BAD_CERTIFICATE_DURATION);
-            }
+            checkDuration(giftCertificate.getDuration());
         }
         validateListOfTags(giftCertificate.getTags());
+    }
+
+    private static void checkDuration(int duration) throws IncorrectUpdateValueException {
+        if (duration < 1 || duration > 365) {
+            throw new IncorrectUpdateValueException(BAD_CERTIFICATE_DURATION);
+        }
+    }
+
+    private static void checkPrice(BigDecimal price) throws IncorrectUpdateValueException {
+        if (price != null && (price.scale() > MAX_SCALE
+                || price.compareTo(MIN_PRICE) < 0 || price.compareTo(MAX_PRICE) > 0)) {
+            throw new IncorrectUpdateValueException(BAD_CERTIFICATE_PRICE);
+        }
+    }
+
+    private static void checkDescription(String description) throws IncorrectUpdateValueException {
+        if (description != null && (description.isBlank() || !Pattern.compile("[\\w\\s]{2,512}").matcher(description).matches())) {
+            throw new IncorrectUpdateValueException(BAD_CERTIFICATE_DESCRIPTION);
+        }
+    }
+
+    private static void checkName(String name) throws IncorrectUpdateValueException {
+        if (name != null && (name.isBlank() || !Pattern.compile("[\\w\\s]{2,128}").matcher(name).matches())) {
+            throw new IncorrectUpdateValueException(BAD_CERTIFICATE_NAME);
+        }
+    }
+
+    private static void checkIfEmpty(GiftCertificate item) throws IncorrectUpdateValueException {
+        if (item.getName() == null
+                && item.getDescription() == null
+                && item.getPrice() == null
+                && item.getDuration() < 1
+                && item.getTags() == null) {
+            throw new IncorrectUpdateValueException(BAD_CERTIFICATE_EMPTY);
+        }
     }
 
     private static void validateListOfTags(List<Tag> tags) throws IncorrectUpdateValueException {

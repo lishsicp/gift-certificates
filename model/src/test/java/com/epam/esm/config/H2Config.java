5ff1248204676fb6335c541bb277bbd1c83cd5d9
Yaroslav Lobur <lobur13@gmail.com>
2023-01-08 15:54:09 +0200

    Added config for embedded h2 tests for repo

---
+++ model/src/test/java/com/epam/esm/config/H2Config.java
@@ -0,0 +1,57 @@
+package com.epam.esm.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("com.epam.esm")
+@PropertySource("classpath:h2.config.properties")
+@Profile("test")
+public class H2Config {
+
+    @Bean
+    public HikariConfig hikariConfig(
+            @Value("${h2.datasource.url}") String url,
+            @Value("${h2.datasource.username}") String username,
+            @Value("${h2.datasource.password}") String password,
+            @Value("${h2.datasource.driver}") String driver,
+            @Value("${h2.datasource.maximumPoolSize}") int maxPoolSize,
+            @Value("${h2.datasource.idleTimeout}") long idleTimeOut
+    ) {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(url);
+        hikariConfig.setUsername(username);
+        hikariConfig.setPassword(password);
+        hikariConfig.setDriverClassName(driver);
+        hikariConfig.setMaximumPoolSize(maxPoolSize);
+        hikariConfig.setIdleTimeout(idleTimeOut);
+        return hikariConfig;
+    }
+
+    @Bean
+    public DataSource dataSource(HikariConfig config) {
+        HikariDataSource dataSource = new HikariDataSource(config);
+        Resource initData = new ClassPathResource("schema.sql");
+        Resource fillData = new ClassPathResource("data.sql");
+        DatabasePopulator databasePopulator = new ResourceDatabasePopulator(initData, fillData);
+        DatabasePopulatorUtils.execute(databasePopulator, dataSource);
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+
+}

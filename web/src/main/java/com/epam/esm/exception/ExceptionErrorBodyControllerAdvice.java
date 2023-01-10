5c3273797398e042bb0a6277fb77f5381dd52b9f
Yaroslav Lobur <lobur13@gmail.com>
2023-01-10 16:22:28 +0200

    Added new handlers. Added some java doc.

--- web/src/main/java/com/epam/esm/exception/ExceptionErrorBodyControllerAdvice.java
+++ web/src/main/java/com/epam/esm/exception/ExceptionErrorBodyControllerAdvice.java
@@ -8,36 +8,48 @@ import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import javax.validation.ConstraintViolationException;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Handles exceptions from controller.
+ * @author Yaroslav Lobur
+ * @version 1.0
+ */
 @ControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class ExceptionErrorBodyControllerAdvice {
 
+    /**
+     * Handles {@link DaoException}
+     */
     @ExceptionHandler(DaoException.class)
     ResponseEntity<ErrorBody> handleDaoException(DaoException ex) {
         String errorMessage = ExceptionMessageLocalizer.toLocale(String.valueOf(ex.getErrorCode()));
+        if (ex.getParameter() != null) errorMessage = String.format(errorMessage, ex.getParameter());
         ErrorBody errorBody = new ErrorBody(errorMessage, ex.getErrorCode());
         return ResponseEntity.status(HttpStatus.valueOf(errorBody.getErrorCode() / 100)).body(errorBody);
     }
 
     /**
-     * Handles exception from <code>javax.validation.Valid</code> annotation
-     * */
+     * Handles exception from {@link javax.validation.Valid} annotation
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         List<ErrorBody> errors = new LinkedList<>();
         ex.getBindingResult().getAllErrors().forEach(error -> {
                     String field = Arrays.stream(((FieldError) error).getField().split("\\.")).reduce((first, second) -> second).orElse("Field");
                     String errorMessage = error.getDefaultMessage();
-                    ErrorBody errorBody = errorBodyMessageSetter(errorMessage, field);
+                    ErrorBody errorBody = errorBodyValidationMessageSetter(errorMessage, field);
                     errors.add(errorBody);
                 }
         );
@@ -45,8 +57,8 @@ public class ExceptionErrorBodyControllerAdvice {
     }
 
     /**
-     * Handles exception from <code>org.springframework.validation.annotation.Validated</code> annotation
-     * */
+     * Handles exception from {@link Validated} annotation
+     */
     @ExceptionHandler
     public ResponseEntity<List<ErrorBody>> handleConstraintViolationException(ConstraintViolationException ex) {
         List<ErrorBody> errors = new LinkedList<>();
@@ -54,13 +66,13 @@ public class ExceptionErrorBodyControllerAdvice {
                 .forEach(e -> {
                     String errorMessage = e.getMessage();
                     String field = e.getPropertyPath().iterator().next().getName();
-                    ErrorBody errorBody = errorBodyMessageSetter(errorMessage, field);
+                    ErrorBody errorBody = errorBodyValidationMessageSetter(errorMessage, field);
                     errors.add(errorBody);
                 });
         return ResponseEntity.badRequest().body(errors);
     }
 
-    private ErrorBody errorBodyMessageSetter(String errorMessage, String errorField) {
+    private ErrorBody errorBodyValidationMessageSetter(String errorMessage, String errorField) {
         ErrorBody errorBody = new ErrorBody();
         if (StringUtils.isNumeric(errorMessage)) {
             int errorCode = Integer.parseInt(errorMessage);
@@ -74,6 +86,9 @@ public class ExceptionErrorBodyControllerAdvice {
         return errorBody;
     }
 
+    /**
+     * Handles {@link DuplicateKeyException}
+     */
     @ExceptionHandler
     public ResponseEntity<ErrorBody> handleDuplicateKeyException(DuplicateKeyException ex) {
         String errorMessage = ExceptionMessageLocalizer.toLocale("40010");
@@ -81,10 +96,33 @@ public class ExceptionErrorBodyControllerAdvice {
         return ResponseEntity.status(HttpStatus.valueOf(errorBody.getErrorCode() / 100)).body(errorBody);
     }
 
+    /**
+     * Handles {@link IncorrectUpdateValueException}
+     */
     @ExceptionHandler
     public ResponseEntity<ErrorBody> handleIncorrectUpdateValueException(IncorrectUpdateValueException ex) {
         String errorMessage = ExceptionMessageLocalizer.toLocale(String.valueOf(ex.getErrorCode()));
         ErrorBody errorBody = new ErrorBody(errorMessage, ex.getErrorCode());
         return ResponseEntity.status(HttpStatus.valueOf(errorBody.getErrorCode() / 100)).body(errorBody);
+    }
+
+    /**
+     * Handles {@link HttpRequestMethodNotSupportedException}
+     */
+    @ExceptionHandler
+    public ResponseEntity<Object> handle(HttpRequestMethodNotSupportedException ex) {
+        String errorMessage = ExceptionMessageLocalizer.toLocale("error.methodNotSupported");
+        ErrorBody errorBody = new ErrorBody(errorMessage, HttpStatus.METHOD_NOT_ALLOWED.value());
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(errorBody);
+    }
+
+    /**
+     * Handles {@link MethodArgumentTypeMismatchException}
+     */
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Object> handle() {
+        String errorMessage = ExceptionMessageLocalizer.toLocale("error.badRequest");
+        ErrorBody errorBody = new ErrorBody(errorMessage, HttpStatus.BAD_REQUEST.value());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorBody);
     }
 }

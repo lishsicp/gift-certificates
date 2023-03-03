79ade24ccc2842c752ab38e1f429775873bdbea6
Yaroslav Lobur <lobur13@gmail.com>
2023-03-03 14:32:23 +0200

    Update spring config

--- module #3. REST API Advanced/src/main/java/com/epam/esm/ApplicationRunner.java
+++ module #3. REST API Advanced/src/main/java/com/epam/esm/ApplicationRunner.java
@@ -2,7 +2,13 @@ package com.epam.esm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableJpaRepositories
+@EnableTransactionManagement
+@EnableJpaAuditing
 @SpringBootApplication
 public class ApplicationRunner {
     public static void main(String[] args) {
--- module #3. REST API Advanced/src/main/java/com/epam/esm/config/SpringConfig.java
+++ module #3. REST API Advanced/src/main/java/com/epam/esm/config/SpringConfig.java
@@ -4,14 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableJpaRepositories
-@EnableTransactionManagement
-@EnableJpaAuditing
 @ComponentScan
 public class SpringConfig {
     @Bean

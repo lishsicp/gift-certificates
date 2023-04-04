a746e044afe07889f1b87568fad72003d3fdb647
Yaroslav Lobur <lobur13@gmail.com>
2023-04-04 15:27:46 +0300

    Delete old auth-server and resource-server

--- module #4. Authentication & Spring Security/auth-server/src/main/java/com/epam/esm/jwt/TokenGenerator.java
+++
@@ -1,9 +0,0 @@
-package com.epam.esm.jwt;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Map;
-
-public interface TokenGenerator {
-    String generateToken(Map<String, Object> customClaims, UserDetails userDetails);
-}
--- module #4. Authentication & Spring Security/auth-server/src/main/resources/db/changelog/0.1-db-structure/db.changelog-0.1.2-insert-initial-data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO auth_user (id, name, email, password, role)
-values (1,'Yaroslav Lobur','lobur13@gmail.com','$2a$12$gi7hwAbZy3eLkCeKMbjxJelPpbj3coEbdBVh6/kEx.F4GD8ymAZhm','ADMIN')
-
-

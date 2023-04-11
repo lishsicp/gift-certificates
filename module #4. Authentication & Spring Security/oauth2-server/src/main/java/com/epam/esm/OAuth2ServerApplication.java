fab26aa1bfee4d28651af60f9911904a325282d4
Yaroslav Lobur <lobur13@gmail.com>
2023-04-11 17:55:19 +0300

    Rename oauth2 server main class

--- module #4. Authentication & Spring Security/oauth2-server/src/main/java/com/epam/esm/OAuth2ServerApplication.java
+++ module #4. Authentication & Spring Security/oauth2-server/src/main/java/com/epam/esm/OAuth2ServerApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GiftCertificateOAuth2ServerApplication {
+public class OAuth2ServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GiftCertificateOAuth2ServerApplication.class, args);
+        SpringApplication.run(OAuth2ServerApplication.class, args);
     }
 }

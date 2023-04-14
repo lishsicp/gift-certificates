e936b7a3e4d2d87d0ab91c70e58c5527c082a4be
Yaroslav Lobur <lobur13@gmail.com>
2023-04-14 20:33:05 +0300

    Add access denied handler

--- module #4. Authentication & Spring Security/oauth2-server/src/main/java/com/epam/esm/config/StandardSecurityConfig.java
+++ module #4. Authentication & Spring Security/oauth2-server/src/main/java/com/epam/esm/config/StandardSecurityConfig.java
@@ -1,13 +1,17 @@
 package com.epam.esm.config;
 
 import com.epam.esm.exception.EntityNotFoundException;
+import com.epam.esm.exception.ExceptionResponseBody;
 import com.epam.esm.idp.FederatedIdentityConfigurer;
 import com.epam.esm.idp.OAuth2TokenClaimsCustomizer;
 import com.epam.esm.idp.UserRepositoryOAuth2UserHandler;
 import com.epam.esm.repository.AuthUserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,7 +23,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
+import org.springframework.util.MimeTypeUtils;
 
 @EnableWebSecurity
 @Configuration
@@ -43,6 +49,7 @@ public class StandardSecurityConfig {
                 .permitAll()
                 .anyRequest()
                 .authenticated())
+            .exceptionHandling(ex -> ex.accessDeniedHandler(accessDeniedHandler()))
             .formLogin(formLogin -> formLogin.usernameParameter("email"));
 
         http.apply(federatedIdentityConfigurer);
@@ -50,8 +57,21 @@ public class StandardSecurityConfig {
         return http.build();
     }
 
+    private AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, accessDeniedException) -> {
+            response.setContentType(MimeTypeUtils.APPLICATION_JSON_VALUE);
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            response.setCharacterEncoding("UTF-8");
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.setPropertyNamingStrategy(new SnakeCaseStrategy());
+            response.getWriter()
+                .write(objectMapper.writeValueAsString(new ExceptionResponseBody(HttpStatus.FORBIDDEN.value(),
+                    "Access is denied, you do not have permission to access this resource")));
+        };
+    }
+
     @Bean
-    public OAuth2TokenCustomizer<JwtEncodingContext> idTokenCustomizer() {
+    public OAuth2TokenCustomizer<JwtEncodingContext> oAuth2TokenCustomizer() {
         return new OAuth2TokenClaimsCustomizer(authUserRepository);
     }
 
